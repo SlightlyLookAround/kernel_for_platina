70ace354680c97ecc937d5e6ac62252498188740
bsp-open <bsp-open@xiaomi.com>
2020-05-28 20:31:43 +0800

    Kernel: Xiaomi kernel changes for MI MAX3 and MI 8Lite Android Q.
    The Patch based on QualComm release TAG: LA.UM.8.2.r1-04300-sdm660.0.
    The kernel config file used are sirius_user_defconfig and grus_user_defconfig.
    
    Change-Id: Ibc6cf55ed7ec08495ed2f45787db11a92b0efedc
    Signed-off-by: bsp-open <bsp-open@xiaomi.com>

--- fs/eventpoll.c
+++ fs/eventpoll.c
@@ -1,6 +1,7 @@
 /*
  *  fs/eventpoll.c (Efficient event retrieval implementation)
  *  Copyright (C) 2001,...,2009	 Davide Libenzi
+ *  Copyright (C) 2020 XiaoMi, Inc.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -1237,7 +1238,9 @@ static int ep_create_wakeup_source(struct epitem *epi)
 	struct wakeup_source *ws;
 
 	if (!epi->ep->ws) {
-		epi->ep->ws = wakeup_source_register("eventpoll");
+		event_name = kasprintf(GFP_KERNEL, "eventpoll-%s", current->comm);
+		epi->ep->ws = wakeup_source_register(event_name);
+		kfree(event_name);
 		if (!epi->ep->ws)
 			return -ENOMEM;
 	}
