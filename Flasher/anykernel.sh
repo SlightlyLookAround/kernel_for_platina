8aaec8b1d62df1705653823b11de0ae498cc1491
Calling <KeternalGithub@163.com>
2023-02-12 08:50:48 +0800

    ci: Migrated-in
    Signed-off-by: Calling <KeternalGithub@163.com>

---
+++ Flasher/anykernel.sh
@@ -0,0 +1,27 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=platina
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+. tools/ak3-core.sh;
+
+dump_boot;
+
+write_boot;
